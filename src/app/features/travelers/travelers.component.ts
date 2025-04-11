f42cb1070001383cd0e04a4fb5f98a58f319fcae
jmalisa <blanjajojo32@gmail.com>
2025-04-11 13:02:22 +0200

    introducing OnPush strategy in traveler feature

--- src/app/features/travelers/travelers.component.ts
+++ src/app/features/travelers/travelers.component.ts
@@ -1,4 +1,10 @@
-import { Component, inject, signal, DestroyRef } from '@angular/core';
+import {
+  Component,
+  inject,
+  signal,
+  DestroyRef,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,7 +14,12 @@ import { MatCardModule } from '@angular/material/card';
 import { Router, RouterLink } from '@angular/router';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { TravelerFormService } from './traveler-form.service';
-import { MONTHS, DAYS, YEARS, COUNTRIES } from '../../shared/constants/constants';
+import {
+  MONTHS,
+  DAYS,
+  YEARS,
+  COUNTRIES,
+} from '../../shared/constants/constants';
 import { TravelerFormData } from './models/traveler.model';
 
 @Component({
@@ -24,6 +35,7 @@ import { TravelerFormData } from './models/traveler.model';
     ReactiveFormsModule,
     RouterLink,
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TravelersComponent {
   private fb = inject(FormBuilder);
@@ -41,7 +53,7 @@ export class TravelersComponent {
     month: ['', [Validators.required]],
     day: ['', [Validators.required]],
     year: ['', [Validators.required]],
-    citizenship: ['', [Validators.required]]
+    citizenship: ['', [Validators.required]],
   });
 
   readonly months = MONTHS;
@@ -50,8 +62,10 @@ export class TravelersComponent {
   readonly countries = COUNTRIES;
   constructor() {
     const navigation = this.router.getCurrentNavigation();
-    const state = navigation?.extras.state as { flightId: string; flightPrice: number } || null;
-    
+    const state =
+      (navigation?.extras.state as { flightId: string; flightPrice: number }) ||
+      null;
+
     if (!state) {
       this.router.navigate(['/']);
       return;
@@ -68,7 +82,7 @@ export class TravelersComponent {
 
     this.travelerForm.valueChanges
       .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(formValue => {
+      .subscribe((formValue) => {
         if (this.travelerForm.dirty) {
           this.travelerFormService.saveForm(formValue as TravelerFormData);
         }
@@ -81,7 +95,7 @@ export class TravelersComponent {
         ...this.travelerForm.value,
         flightId: this.flightId(),
       };
-      
+
       console.log('Booking Details:', formData);
 
       const alertMessage = `
@@ -93,16 +107,43 @@ export class TravelersComponent {
         First Name: ${this.travelerForm.value.firstName}
         Last Name: ${this.travelerForm.value.lastName}
         Gender: ${this.travelerForm.value.gender}
-        Birth Date: ${this.travelerForm.value.month} ${this.travelerForm.value.day}, ${this.travelerForm.value.year}
+        Birth Date: ${this.travelerForm.value.month} ${
+        this.travelerForm.value.day
+      }, ${this.travelerForm.value.year}
         Citizenship: ${this.travelerForm.value.citizenship}
       `;
 
       alert(alertMessage);
-      
+
       this.travelerForm.reset();
       // Clear saved form data after successful submission
       this.travelerFormService.clearForm();
     }
   }
-}
 
+
+  get firstNameHasError(): boolean {
+    return this.travelerForm.controls['firstName'].hasError('required');
+  }
+
+  get lastNameHasError(): boolean {
+    return this.travelerForm.controls['lastName'].hasError('required');
+  }
+  
+  get monthHasError(): boolean {
+    return this.travelerForm.controls['month'].hasError('required');
+  }
+  
+  get dayHasError(): boolean {
+    return this.travelerForm.controls['day'].hasError('required');
+  }
+  
+  get yearHasError(): boolean {
+    return this.travelerForm.controls['year'].hasError('required');
+  }
+  
+  get citizenshipHasError(): boolean {
+    return this.travelerForm.controls['citizenship'].hasError('required');
+  }
+
+}
